13c4eaffc0bbcb8763851016c9449d4d0105ebd3
riyazedd <riyaz.shrestha00@gmail.com>
2024-04-01 09:11:06 +0545

    User Controller Added

---
+++ backend/src/controller/UserController.js
@@ -0,0 +1,23 @@
+class UserController{
+    async index(req,res){
+
+    }
+
+    async show(req,res){
+
+    }
+
+    async store(req,res){
+        
+    }
+
+    async update(req,res){
+
+    }
+
+    async delete(req,res){
+
+    }
+}
+
+export default UserController;
