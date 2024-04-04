099523d8a852c1d552d73299e8ba24a527ddb806
riyazedd <riyaz.shrestha00@gmail.com>
2024-04-04 09:30:27 +0545

    API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import Connection from './database/Connection.js';
 import router from './router/web.js';
+import cors from 'cors';
 
 const app=express();
 new Connection();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(router);
--- frontend/src/components/auth/RegisterComponent.jsx
+++ frontend/src/components/auth/RegisterComponent.jsx
@@ -4,14 +4,14 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import HeaderComponent from '../../layouts/HeaderComponent';
 import FooterComponent from '../../layouts/FooterComponent';
+import API from '../../API';
 
 const registerSchema = yup.object().shape({
     name: yup.string().required(),
     email: yup.string().email().required(),
     password: yup.string().required(),
     password_confirm: yup.string().required().oneOf([yup.ref('password')], 'Password confirm must be same as password'),
-    gender: yup.string().required(),
-    role: yup.string().required(),
+    gender: yup.string().required()
 });
 
 export default function RegisterComponent() {
@@ -20,8 +20,14 @@ export default function RegisterComponent() {
     });
 
     const registerRecord = (data) => {
-        console.log(data);
+        API.post('users',data).then(response=>{
+            console.log(response);
+        }).catch(error=>{
+            console.log(error);
+        });
     }
+
+
     return (
         <div>
         <HeaderComponent />
@@ -63,7 +69,7 @@ export default function RegisterComponent() {
                                     className="form-control" />
                             </div>
                             <div className="form-group mb-2">
-                                <label htmlFor="password_confirm">Password Confirm: &nbsp;
+                                <label htmlFor="password_confirm">Confirm Password: &nbsp;
                                     <a className='text-danger'>
                                         {errors.password_confirm?.message && <span>{errors.password_confirm?.message}</span>}
                                     </a>
@@ -82,19 +88,6 @@ export default function RegisterComponent() {
                                     <option value="female">Female</option>
 
                                 </select>
-                            </div>
-                            <div className="form-group mb-2">
-                                <label> Role:&nbsp;
-                                    <a className='text-danger'>
-                                        {errors.role?.message && <span>{errors.role?.message}</span>}
-                                    </a>
-                                </label>
-                                <select name='role'  {...register("role")} className="form-control">
-                                    <option value=''>Select Role </option>
-                                    <option value="admin">Admin</option>
-                                    <option value="user">User</option>
-                                </select>
-
                             </div>
 
                             <div className="form-group mb-2">
